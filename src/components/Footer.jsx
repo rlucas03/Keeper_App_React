ae56c04777bdbaa466cdd6d922408e044ab0af31
Ryan Lucas <daytripper@pounces-imac-2.home>
2022-08-03 21:15:00 +0100

    edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright  ⓒ {year}</p>
+      <p>Copyright  ⓒ {year} Made with ❤️ by Ryan Lucas</p>
     </footer>
   );
 }
